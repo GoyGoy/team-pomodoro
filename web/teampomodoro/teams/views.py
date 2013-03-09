a1904370ae4be1436f2ab808ff8153a0bc21cbdc
fatiherikli <fatiherikli@gmail.com>
2013-03-09 18:14:35 +0200

    Added login and registration tempalte

--- web/teampomodoro/teams/views.py
+++ web/teampomodoro/teams/views.py
@@ -1,5 +1,7 @@
 from django.views.generic import TemplateView
 
+from profiles.mixins import LoginRequiredMixin
 
-class HomeView(TemplateView):
+
+class HomeView(LoginRequiredMixin, TemplateView):
     template_name = 'index.html'
