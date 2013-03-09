9227790629e20ece82027dc4001a2ea31133893a
fatiherikli <fatiherikli@gmail.com>
2013-03-09 15:06:46 +0200

    Added pomodoro app

--- web/teampomodoro/teams/models.py
+++ web/teampomodoro/teams/models.py
@@ -1,5 +1,19 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.utils.encoding import smart_unicode
+from django.utils.translation import ugettext_lazy as _
 
 
 class Team(models.Model):
+    """
+    Holds teams
+    """
     name = models.CharField(max_length=255)
+    owner = models.ForeignKey(User, related_name=_("Owner"))
+    members = models.ManyToManyField(User, related_name="teams")
+    pomodoro_time = models.PositiveIntegerField(_("Pomodoro Time"))
+    long_break_time = models.PositiveIntegerField(_("Long Break Time"))
+    short_break_time = models.PositiveIntegerField(_("Short Break Time"))
+
+    def __unicode__(self):
+        return smart_unicode(self.name)
